ff5efe9a50e036ce1283ff666b7d1128f7b7f1b4
Alisha Antony <36256101+alishaantony@users.noreply.github.com>
2019-09-13 21:48:32 +0530

    Create staircase.c

---
+++ staircase.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+            printf(" ");
+        }
+        for(k=0;k<i+1;k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
